47ffa4a665a9917eee90c14bc3dd56ae0e71506b
JanWeil <ja1610@yahoo.com>
2022-06-29 17:54:09 +0200

    sign in anon angefangen

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -2,13 +2,24 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
 
-
 final FirebaseAuth  _auth = FirebaseAuth.instance;
 
-//sign in with email & password
+  //sign in anon
+  Future signInAnon() async {
+    try {
+      AuthResult result = await _auth.signInAnonymously();
+      FirebaseUser user = result.user;
+      return user;
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
-//register with email & password
+  //sign in with email & password
 
-//sign out
+  //register with email & password
+
+  //sign out
 
 }
